6abfcd189b0574d61052d4e598049c35c9a45f82
Nandini Raut <rautnandini2001@gmail.com>
2021-02-08 23:01:20 +0530

    Static functions edit

---
+++ Static.java
@@ -0,0 +1,15 @@
+class Static
+{
+   
+    static void staticFun()
+    
+    {
+        System.out.println("APPLE");
+    }
+ 
+    public static void main(String[] args)
+    {
+         
+           staticFun();
+    }
+}
