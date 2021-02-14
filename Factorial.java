22a95c3193dbe482f7d1c7699128951bb542b2cf
Nandini Raut <rautnandini2001@gmail.com>
2021-02-14 23:05:40 +0530

    Factorial Code Edit 2

--- Factorial.java
+++ Factorial.java
@@ -1,7 +1,10 @@
 class Factorial{  
- public static void main(String args[]){  
-  int i,fact=1;  
-  int number=5;//It is the number to calculate factorial    
+
+	public static void main(String args[]){  
+  
+		int i,fact=1;  
+  
+		int number=5;    
   for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
