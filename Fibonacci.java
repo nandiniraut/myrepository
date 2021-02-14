7092b185fa31857e575a269d671f3bb59b446ba4
Nandini Raut <rautnandini2001@gmail.com>
2021-02-14 23:03:19 +0530

    Fibonacci Code Edit 2

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,8 @@
 public class Fibonacci {  
-    static int n1=0,n2=1,n3=0;      
-     static void printFibo(int count){      
+
+	static int n1=0,n2=1,n3=0;      
+     
+	static void printFibo(int count){      
         if(count>0){      
              n3 = n1 + n2;      
              n1 = n2;      
