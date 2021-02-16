7c6f35e1d0225e35935e67074594d138541ff4ca
Nandini Raut <rautnandini2001@gmail.com>
2021-02-16 13:37:06 +0530

    Doccumentation in Programmer.java

--- Programmer.java
+++ Programmer.java
@@ -1,10 +1,10 @@
 class Employee{  
  float salary=40000;  
 }  
-class Programmer extends Employee{  
+class Programmer extends Employee{ // "extends" keyword is used for inheritance 
  int bonus=10000;  
  public static void main(String args[]){  
-   Programmer p=new Programmer();  
+   Programmer p=new Programmer();  // object is created with "new" keyword
    System.out.println("Programmer salary is:"+p.salary);  
    System.out.println("Bonus of Programmer is:"+p.bonus);  
 }  
