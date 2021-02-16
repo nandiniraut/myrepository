3117a4dbdd33b30c11c276c1747b9754c7108d93
Nandini Raut <rautnandini2001@gmail.com>
2021-02-16 13:31:03 +0530

    Doccumentation Done in Addition of Two Nos

--- UserInputForAdd.java
+++ UserInputForAdd.java
@@ -3,13 +3,13 @@ class UserInputForAdd
 {  
 public static void main(String[] args)  
 {  
-Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+Scanner sc= new Scanner(System.in);    //System.in is a standard input stream, scanner is the syntax to take input from user  
 System.out.print("Enter first number- ");  
-int a= sc.nextInt();  
+int a= sc.nextInt();  //taking first number from user
 System.out.print("Enter second number- ");  
-int b= sc.nextInt();  
+int b= sc.nextInt();  //taking second number from user
 System.out.print("Enter third number- ");  
-int c= sc.nextInt();  
+int c= sc.nextInt();  //the addition will be stored in this
 int d=a+b+c;  
 System.out.println("Total= " +d);  
 }  
