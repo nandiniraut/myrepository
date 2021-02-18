0d6110db4ece8665a0e1c4c0a004da49edecf734
Nandini Raut <rautnandini2001@gmail.com>
2021-02-18 20:58:58 +0530

    Edit Test.java

--- Test.java
+++ Test.java
@@ -6,27 +6,27 @@ class Box
 	
 	Box(double w, double h, double d)  //1st constructor
 	{ 
-		width = w; 
-		height = h; 
-		depth = d; 
+	    width = w; 
+	    height = h; 
+	    depth = d; 
 	} 
 
 	 
 	Box()  // second constructor
 	{ 
-		width = height = depth = 0; 
+	    width = height = depth = 0; 
 	} 
 
 	 
 	Box(double len)  // third constructor, overloading happens here
 	{ 
-		width = height = depth = len; 
+	    width = height = depth = len; 
 	} 
 
 	
 	double volume() //function to calculate volume
 	{ 
-		return width * height * depth; 
+	    return width * height * depth; 
 	} 
 } 
 
@@ -35,22 +35,22 @@ public class Test
 { 
 	public static void main(String args[]) 
 	{ 
-		Box mybox1 = new Box(10, 20, 15); 
-		Box mybox2 = new Box(); 
-		Box mycube = new Box(7); 
+	    Box mybox1 = new Box(10, 20, 15); 
+	    Box mybox2 = new Box(); 
+	    Box mycube = new Box(7); 
 
-		double vol; 
-
-		
-		vol = mybox1.volume(); 
-		System.out.println(" Volume of mybox1 is " + vol); 
+	    double vol; 
 
 		
-		vol = mybox2.volume(); 
-		System.out.println(" Volume of mybox2 is " + vol); 
+	    vol = mybox1.volume(); 
+	    System.out.println(" Volume of mybox1 is " + vol); 
 
 		
-		vol = mycube.volume(); 
-		System.out.println(" Volume of mycube is " + vol); 
+	    vol = mybox2.volume(); 
+	    System.out.println(" Volume of mybox2 is " + vol); 
+
+		
+	    vol = mycube.volume(); 
+	    System.out.println(" Volume of mycube is " + vol); 
 	} 
 } 
