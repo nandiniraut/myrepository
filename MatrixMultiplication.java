77f55fcdcf4bbe4060532658d3de607a747d1860
Nandini Raut <rautnandini2001@gmail.com>
2021-02-24 22:48:08 +0530

    ...

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -8,7 +8,7 @@ int b[][]={{1,1,1},{2,2,2},{3,3,3}};
 	//creating another matrix to store the multiplication of two matrices    
 int c[][]=new int[3][3];  //3 rows and 3 columns  
     
-//multiplying and printing multiplication of 2 matrices    
+	//multiplying and printing multiplication of 2 matrices    
 for(int i=0;i<3;i++){    
 for(int j=0;j<3;j++){    
 c[i][j]=0;      
