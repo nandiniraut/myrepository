3849a22303873d693dd429dec58cdade0806fbb2
Nandini Raut <rautnandini2001@gmail.com>
2021-02-26 23:07:30 +0530

    Edit

--- Lcm.java
+++ Lcm.java
@@ -6,7 +6,7 @@ public class Lcm {
     
     lcm = (n1 > n2) ? n1 : n2;
 
-
+i
     while(true) {
       if( lcm % n1 == 0 && lcm % n2 == 0 ) {
         System.out.printf("The LCM of %d and %d is %d.", n1, n2, lcm);
