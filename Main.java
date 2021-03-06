5081f1ecbed14ac1be2c4d251ed03beed05c107b
Nandini Raut <rautnandini2001@gmail.com>
2021-03-06 23:05:21 +0530

    ..

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
     
-	i	Person myObj = new Person(); // uses the previous file person.java
+		Person myObj = new Person(); // uses the previous file person.java
     myObj.setName("John"); // Set the value of the name variable to whatever name we specify here
 
     System.out.println(myObj.getName());
