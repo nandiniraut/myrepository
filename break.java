36eb2ea27b9f60d3ee851eb2b51108d884551381
Nandini Raut <rautnandini2001@gmail.com>
2021-03-10 22:51:44 +0530

    break.java edited

--- break.java
+++ break.java
@@ -1,9 +1,7 @@
 public class Break {  
-public static void main(String[] args) {  
-    //using for loop  
+public static void main(String[] args) {    
     for(int i=1;i<=10;i++){  
         if(i==5){  
-            //breaking the loop  
             break;  
         }  
         System.out.println(i);  
