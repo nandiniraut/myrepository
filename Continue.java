89e5f8c4305f50495fc9b71529998c7cf16c6d60
Nandini Raut <rautnandini2001@gmail.com>
2021-03-12 22:12:37 +0530

    doccumentation added in Continue.java

--- Continue.java
+++ Continue.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
     for(int i=1;i<=10;i++){  
         if(i==5){  
               
-            continue;  
+            continue; // it will continue the loop  
         }  
         System.out.println(i);  
     }  
