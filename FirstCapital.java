642152f1c5b8a22fee870d0d5aeecf63a951bdf8
Nandini Raut <rautnandini2001@gmail.com>
2021-04-29 22:58:43 +0530

    java code that makes all first letter capital in each word

---
+++ FirstCapital.java
@@ -0,0 +1,16 @@
+
+ public class FirstCapital {
+     public static void main(String[] args){
+         System.out.println("Program by Nandini Raut, roll number R171219019");
+	 Scanner in = new Scanner(System.in);
+         System.out.print("Enter your sentence");
+	 String sentence = in.nextLine();
+	 String struppercase = ""; 
+         Scanner sentenceScan = new Scanner(sentence); 
+         while(sentenceScan.hasNext()) {
+             String word = sentenceScan.next(); 
+             struppercase += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " "; 
+         }
+      System.out.println(struppercase.trim()); 
+    }
+}
