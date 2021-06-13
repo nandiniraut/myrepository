46b1d947f4e03edb1673e1d086375417c332255b
Nandini Raut <rautnandini2001@gmail.com>
2021-06-13 22:36:46 +0530

    greatestbw2.cpp

---
+++ codes/greatestbw2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"enter the two numbers ";
+	cin>>a>>b;
+	if(a>b)
+	{
+		cout<<a<<" is greater than "<<b;
+	}
+	else
+	{
+		cout<<b<<" is greater than "<<a;
+	}
+	
+	return 0;
+}
