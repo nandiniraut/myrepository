56cc1f70dc1e4314052f13b64862cd0dc79739a2
Nandini Raut <rautnandini2001@gmail.com>
2021-06-14 23:48:36 +0530

    addfive.cpp

---
+++ codes in c++/addfive.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c,d,e,f;
+	cout<<"enter the 5 numbers"<<endl;
+	cin>>a>>b>>c>>d>>e;
+	f=a+b+c+d+e;
+	cout<<"the sum is"<<endl<<f;
+	
+	return 0;
+}
